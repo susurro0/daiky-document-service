5b744b298c8c4b743ae93db98fa216c1eef9c377
denizcingoez <us.cingoez@gmail.com>
2025-01-11 18:05:57 -0500

    set up file upload

--- tests/api/schemas/test_document_schemas.py
+++ tests/api/schemas/test_document_schemas.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import pytest
+from fastapi import UploadFile
 from pydantic import ValidationError
 from enum import Enum
 
@@ -8,13 +9,15 @@ from app.api.schemas.document_schemas import DocumentBase, DocumentCreate, Docum
 
 
 def test_document_base_model_valid():
-    document = DocumentBase(file_name="Sample Document", file_type="This is a document", upload_timestamp=datetime(2022, 1, 1))
-    assert document.file_name == "Sample Document"
-    assert document.file_type == "This is a document"
+    document = DocumentBase(file_name="test_file.txt", file_type="text/plain", upload_timestamp=datetime(2022, 1, 1))
+    assert document.file_name == "test_file.txt"
+    assert document.file_type == "text/plain"
     assert document.upload_timestamp == datetime(2022, 1, 1)
 
 def test_document_create_model():
-    document_create = DocumentCreate(file_name="test_file.txt", file_type="text/plain", upload_timestamp=datetime(2022, 1, 1))
+    file = UploadFile(filename="test.txt", file=b"Hello, World!")
+
+    document_create = DocumentCreate(file=file, file_name="test_file.txt", file_type="text/plain", upload_timestamp=datetime(2022, 1, 1))
     assert document_create.file_name == "test_file.txt"
     assert document_create.file_type == "text/plain"
     assert document_create.upload_timestamp == datetime(2022, 1, 1)
